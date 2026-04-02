be7a9989cb988b90690a4e36c915befe3c1a9d0e
yassin fhm <yassin.fahe12@gmail.com>
2026-04-02 15:45:14 +0200

    delete account

---
+++ config/firebase_example.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// auth
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+// db
+export const firestore = getFirestore(app);
---
+++ constants/index_example.ts
@@ -0,0 +1,2 @@
+export const CLOUDINARY_CLOUD_NAME  = "";
+export const CLOUDINARY_UPLOAD_PRESET = "";
