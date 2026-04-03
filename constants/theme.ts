843e85aaa5b08e0ad56615fd150aaf857a94ee30
yassin fhm <yassin.fahe12@gmail.com>
2026-04-03 21:01:32 +0200

    fix: theme

--- constants/theme.ts
+++ constants/theme.ts
@@ -1,7 +1,7 @@
 import { scale, verticalScale } from "@/utils/styling";
 
 export const colors = {
-  primary: "#a3e635",
+  primary: "#FDF7FA",
   primaryLight: "#0ea5e9",
   primaryDark: "#0369a1",
   text: "#fff",
